ac9ad72d874c1a5b8f49e5e00ec177bb5a78196d
Yadira Cceline Auccasi Loa <yadira.auccasiloa@gmail.com>
2025-05-15 12:02:26 -0500

    agregar editar mesa

--- src/app/mesas/editar-mesa/editar-mesa.component.ts
+++ src/app/mesas/editar-mesa/editar-mesa.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
-import { TituloPrincipalComponent } from "../../compartidos/titulo-principal/titulo-principal.component";
+import { Component, inject } from '@angular/core';
+import { TituloPrincipalComponent } from '../../compartidos/titulo-principal/titulo-principal.component';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
+import { MatRadioModule } from '@angular/material/radio';
 
 @Component({
   selector: 'app-editar-mesa',
-  imports: [TituloPrincipalComponent],
+  imports: [
+    TituloPrincipalComponent,
+    MatSelectModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    RouterLink,
+    MatRadioModule
+  ],
   templateUrl: './editar-mesa.component.html',
-  styleUrl: './editar-mesa.component.css'
+  styleUrl: './editar-mesa.component.css',
 })
 export class EditarMesaComponent {
+  private formBuilder = inject(FormBuilder);
 
+  form = this.formBuilder.group({
+    numero: [''],
+    capacidad: [0],
+    ubicacionId: [0],
+    estado: [true],
+  });
+
+  ubicaciones = [
+    { id: 1, nombre: 'Esquina' },
+    { id: 2, nombre: 'Centro' },
+    { id: 3, nombre: 'Pared' },
+    { id: 4, nombre: 'Ventana' },
+  ];
+
+  guardarCambios() {
+    console.log(this.form.value)
+  }
 }
