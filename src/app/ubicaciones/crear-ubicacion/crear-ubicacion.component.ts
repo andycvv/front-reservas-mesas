972014eccf4003f27a638ba1cd240b3cc70b3b11
Andy Camilo Vargas Vargas <135892408+andycvv@users.noreply.github.com>
2025-05-15 23:26:38 -0500

    agregar crear ubicacion

--- src/app/ubicaciones/crear-ubicacion/crear-ubicacion.component.ts
+++ src/app/ubicaciones/crear-ubicacion/crear-ubicacion.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TituloPrincipalComponent } from "../../compartidos/titulo-principal/titulo-principal.component";
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-crear-ubicacion',
-  imports: [],
+  imports: [
+    TituloPrincipalComponent,
+    MatSelectModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    RouterLink
+  ],
   templateUrl: './crear-ubicacion.component.html',
   styleUrl: './crear-ubicacion.component.css'
 })
 export class CrearUbicacionComponent {
+  private formBuilder = inject(FormBuilder);
+  form = this.formBuilder.group({
+    nombre: ['']
+  });
 
+  guardarCambios() {
+    console.log(this.form.value)
+  }
 }
