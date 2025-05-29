96fefb6d03798766d70bd2d15f0aeb54bbcf1c49
Andy Camilo Vargas Vargas <135892408+andycvv@users.noreply.github.com>
2025-05-28 19:15:56 -0500

    modificar menu para cambiar el boton de reserva a un menu desplegable de acciones para las reservas

--- src/app/compartidos/menu/menu.component.ts
+++ src/app/compartidos/menu/menu.component.ts
@@ -3,10 +3,11 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-menu',
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterLink],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterLink, MatMenuModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css',
 })
