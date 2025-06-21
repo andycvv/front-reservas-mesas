3bec4eaa4e35dac65fbd903122affffe38a38ed9
Andy Camilo Vargas Vargas <135892408+andycvv@users.noreply.github.com>
2025-06-21 14:27:28 -0500

    modificar menu

--- src/app/compartidos/menu/menu.component.ts
+++ src/app/compartidos/menu/menu.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
 import { MatMenuModule } from '@angular/material/menu';
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-menu',
@@ -11,4 +12,18 @@ import { MatMenuModule } from '@angular/material/menu';
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css',
 })
-export class MenuComponent {}
+export class MenuComponent implements OnInit {
+  private authService = inject(AuthService)
+  isLogged = false;
+  roles: string[] = [];
+
+  ngOnInit(): void {
+    this.authService.cargarEstadoDesdeLocalStorage();
+    this.authService.isLogged$.subscribe(isLogged => this.isLogged = isLogged)
+    this.authService.roles$.subscribe(roles => this.roles = roles)
+  }
+  
+  salir() {
+    this.authService.logout()
+  }
+}
