83a68dcd59168937ae3e7f92cfef00836984d6dc
Andy Camilo Vargas Vargas <135892408+andycvv@users.noreply.github.com>
2025-06-21 14:27:42 -0500

    agregar configuracion para los interceptores y nuevas rutas

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,14 +3,15 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './auth/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: 
   [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes, withComponentInputBinding()),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([authInterceptor])),
     importProvidersFrom([SweetAlert2Module.forRoot()])
   ]
 };
